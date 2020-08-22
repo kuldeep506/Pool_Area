dd0ed5b8160d27ef8976ec0ba2d4129f58ff2be1
kuldeep506 <67869264+kuldeep506@users.noreply.github.com>
2020-08-22 19:20:46 +0530

    method name getArea() created

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -6,35 +6,38 @@
  */
 
 public class Rectangle {
-private double width;
-private double length;
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        if (width < 0) {
+            this.width = 0;
+        }
+        this.length = length;
+        if (length < 0) {
+            this.length = 0;
+        }
+
+    }
 
     public double getWidth() {
         return width;
-    }
-
-    public double getLength() {
-        return length;
     }
 
     public void setWidth(double width) {
         this.width = width;
     }
 
+    public double getLength() {
+        return length;
+    }
+
     public void setLength(double length) {
         this.length = length;
     }
 
-    public Rectangle(double width, double length) {
-        this.width = width;
-        if (width<0){
-            this.width=0;
-        }
-        this.length = length;
-        if (length<0){
-            this.length=0;
-        }
-
+    public double getArea() {
+        return width * length;
     }
-
 }
