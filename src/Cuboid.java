570f8f35370bf2f5046ea27ae36ba045f542fe6d
kuldeep506 <67869264+kuldeep506@users.noreply.github.com>
2020-08-22 19:26:13 +0530

    method getHeight() created

--- src/Cuboid.java
+++ src/Cuboid.java
@@ -7,13 +7,17 @@
 
 public class Cuboid extends Rectangle {
     private double height;
-    public Cuboid(double width, double length,double height) {
+
+
+
+    public Cuboid(double width, double length, double height) {
         super(width, length);
         this.height= height;
         if (height<0){
             this.height=0;
         }
-
     }
-
+    public double getHeight() {
+        return height;
+    }
 }
