3c17642085f07ca452fbce266ff1d9a698ba16af
kuldeep506 <67869264+kuldeep506@users.noreply.github.com>
2020-08-22 19:15:29 +0530

    initial commit Rectangle.java created

---
+++ src/Rectangle.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kuldeep Singh (kuldeep506)
+ *   Date: 8/22/2020
+ *   Time: 7:12 PM
+ *   File: Ractangle.java
+ */
+
+public class Rectangle {
+private double width;
+private double length;
+
+}
